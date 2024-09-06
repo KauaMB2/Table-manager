6470f9a2cc3cdad19d5173a16af6a9531a727162
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-09-05 22:51:05 -0300

    Changed the table style

--- src/components/PreviewPanel.tsx
+++ src/components/PreviewPanel.tsx
@@ -2,7 +2,10 @@ import { useContext } from "react"
 import { ColorsContext } from "../contexts/Color"
 import addColumnIcon from "./../assets/images/addColumnIcon.svg"
 import deleteColumnIcon from "./../assets/images/deleteColumnIcon.svg"
+import arrowLeft from "./../assets/images/arrowLeft.svg"
+import arrowRight from "./../assets/images/arrowRight.svg"
 import { TablesContext } from "../contexts/Tables"
+
 
 const PreviewPanel = () => {
   const { primaryColor, secondaryColor, textColor } = useContext(ColorsContext)
@@ -13,26 +16,26 @@ const PreviewPanel = () => {
           <header style={{ backgroundColor: primaryColor}} className="tableContainer-header">
             <h5 className="tableContainer-header-title">{currentTable.tableName}</h5>
           </header>
-          <table style={{ backgroundColor: secondaryColor}}>
-            <thead style={{ backgroundColor: secondaryColor}}>
-              <tr>
+          <div className="table" style={{ backgroundColor: primaryColor}}>
+            <div style={{ backgroundColor: primaryColor}} className="tableContainer-body">
                 {
                   currentTable.content.map((column, index) => {
-                    return <th key={index}>{column[0]}</th>
+                    return (
+                      <div key={index} id={index.toString()} className="tableContainer-cell">
+                        <div style={{ backgroundColor: secondaryColor}} className="tableContainer-body-columnName">
+                          <span>{column[0]}</span>
+                          <div style={{ backgroundColor: secondaryColor}} className="arrowsDiv">
+                            <img width={18} height={18} src={arrowLeft} />
+                            <img width={18} height={18} src={arrowRight} />
+                          </div>
+                        </div>
+                        <div style={{ backgroundColor: primaryColor}} className="tableContainer-body-content">{column[1]}</div>
+                      </div>
+                    )
                   })
                 }
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                {
-                  currentTable.content.map((column, index) => (
-                    <td key={index}>{column[1]}</td>
-                  ))
-                }
-              </tr>
-            </tbody>
-          </table>
+            </div>
+          </div>
           <footer style={{ backgroundColor: primaryColor}} className="tableContainer-footer">
             <button onClick={removeColumn}><img src={deleteColumnIcon} className="removeColumnIcon"/> Remove column</button>
             <button onClick={addColumn}><img src={addColumnIcon} className="addColumnIcon"/> Add column</button>
